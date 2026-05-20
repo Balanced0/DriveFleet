d7717f2b0fdaf987b098d4eb8076b37b9fe59615
balanced0 <alvichowdhury013@gmail.com>
2026-05-20 18:50:38 +0600

    feat: enhanced login page layout and added branding elements; included Car icon and updated hero section for improved user experience

--- src/app/(Auth)/login/page.jsx
+++ src/app/(Auth)/login/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import authBG from "@/assets/auth-bg.jpg";
 import { Check } from "@gravity-ui/icons";
+import { Car } from "lucide-react";
 import {
   Button,
   Description,
@@ -22,17 +23,46 @@ const page = () => {
     alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
   };
   return (
-    <div className="grid grid-cols-2"> 
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <div
-        className="hero min-h-screen"
+        className="hero min-h-screen flex items-start justify-start relative hidden lg:grid"
         style={{
           backgroundImage: `url(${authBG.src})`,
         }}
       >
-        <div className="hero-overlay"></div>
+        <div className="flex gap-2 items-center p-10">
+          <div className="bg-orange-500 p-2 rounded-2xl">
+            <Car className="text-black" />
+          </div>
+          <a className="text-xl">
+            <span className="font-extrabold">Drive</span>
+            <span className="font-extrabold text-orange-500">Fleet</span>
+          </a>
+        </div>
+
+        <div className="absolute bottom-10 left-10">
+          <h1 className="text-4xl font-extrabold mb-4">
+            Your next ride is one tap away.
+          </h1>
+          <p className="text-lg text-gray-400">
+            Join 50,000+ drivers using DriveFleet to rent premium cars
+            worldwide.
+          </p>
+        </div>
       </div>
-      <div className="flex items-center justify-center p-4">
+      <div className="flex flex-col items-center justify-center p-4 bg-black min-h-screen">
         <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+          <div className="flex gap-2 items-center mb-8 lg:hidden">
+            <div className="bg-orange-500 p-2 rounded-2xl">
+              <Car className="text-black" />
+            </div>
+            <a className="text-xl">
+              <span className="font-extrabold">Drive</span>
+              <span className="font-extrabold text-orange-500">Fleet</span>
+            </a>
+          </div>
+          <h1 className="text-3xl font-extrabold">Welcome Back</h1>
+          <p className="text-gray-400">Login to continue your journey.</p>
           <TextField
             isRequired
             name="email"
