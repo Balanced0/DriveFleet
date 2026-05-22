aed8c51fc1e8b54934f3d97de6db523ba13407c6
balanced0 <alvichowdhury013@gmail.com>
2026-05-22 20:20:12 +0600

    feat: improved layout and styling of AddedCarsPage for better responsiveness

--- src/app/(Main)/added/page.jsx
+++ src/app/(Main)/added/page.jsx
@@ -12,25 +12,25 @@ const AddedCarsPage = async () => {
   return (
     <div className="bg-black">
       <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center pt-20 mb-8">
+        <div className="flex flex-col gap-4 pt-20 mb-8 md:flex-row md:justify-between md:items-center">
           <div>
-            <h2 className="text-5xl font-extrabold mb-3">My added Cars</h2>
+            <h2 className="text-3xl font-extrabold mb-3 md:text-5xl">
+              My added Cars
+            </h2>
             <p className="text-gray-400 text-lg">Manage your listings.</p>
           </div>
           <div>
             <Link href="add">
-              <button className="btn bg-orange-500 text-black rounded-xl">
+              <button className="btn bg-orange-500 text-black rounded-xl w-full md:w-auto">
                 <Plus /> Add new car
               </button>
             </Link>
           </div>
         </div>
         <div className="grid grid-cols-1 gap-6 pb-20 md:grid-cols-2 lg:grid-cols-3">
-            {
-                data.map((car) =>(
-                    <AddedCarCard key={car._id} car={car}></AddedCarCard>
-                ))
-            }
+          {data.map((car) => (
+            <AddedCarCard key={car._id} car={car}></AddedCarCard>
+          ))}
         </div>
       </div>
     </div>
