08d70ad1b4cc6843769ae8d5e1a198c7bc75ab62
balanced0 <alvichowdhury013@gmail.com>
2026-05-23 12:33:56 +0600

    feat: added filter form component for car search functionality in ExploreCarsPage

--- src/app/(Main)/explore/page.jsx
+++ src/app/(Main)/explore/page.jsx
@@ -1,19 +1,35 @@
 import CarCard from "@/components/CarCard";
 import React from "react";
+import FilterForm from "@/components/FilterForm";
+import { SlidersHorizontal } from "lucide-react";
 
-const ExploreCarsPage = async () => {
-  const res = await fetch("http://localhost:5000/cars");
+const ExploreCarsPage = async ({ searchParams }) => {
+  const { carName, carType } = await searchParams;
+  const url = new URL("http://localhost:5000/cars/search");
+  if (carName) {
+    url.searchParams.set("carName", carName);
+  }
+  if (carType) {
+    url.searchParams.set("carType", carType);
+  }
+  const res = await fetch(url.toString());
   const data = await res.json();
   return (
     <div className="bg-black">
       <div className="container mx-auto px-4">
-        <h1 className="pt-20 text-4xl font-extrabold mb-12">Explore the fleet</h1>
+        <h1 className="pt-20 text-4xl font-extrabold mb-4">
+          Explore the fleet
+        </h1>
+        <div className="border border-gray-600 p-6 rounded-2xl mb-12 bg-[#1D232A]">
+          <p className="flex gap-2 items-center font-bold mb-3">
+            <SlidersHorizontal className="text-orange-500" /> Filter
+          </p>
+          <FilterForm></FilterForm>
+        </div>
         <div className="grid grid-cols-1 gap-6 pb-20 md:grid-cols-2 lg:grid-cols-3">
-            {
-                data.map((car) =>(
-                    <CarCard key={car._id} car={car}></CarCard>
-                ))
-            }
+          {data.map((car) => (
+            <CarCard key={car._id} car={car}></CarCard>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/FilterForm.jsx
@@ -0,0 +1,92 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import {
+  Button,
+  FieldError,
+  Input,
+  Label,
+  TextField,
+  Select,
+  ListBox,
+  Form,
+} from "@heroui/react";
+
+const FilterForm = () => {
+  const router = useRouter();
+  const [carType, setCarType] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const carName = formData.get("carName");
+    router.push(`/explore?carName=${carName}&carType=${carType}`);
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <TextField name="carName">
+          <Label className="flex items-center gap-2 text-gray-400">
+            Car Name
+          </Label>
+          <Input
+            placeholder="e.g. Onyx GT Coupe"
+            className="border border-gray-600 focus:border-orange-500"
+          />
+          <FieldError />
+        </TextField>
+
+        <Select
+          placeholder="Select one"
+          value={carType}
+          onChange={setCarType}
+        >
+          <Label className="flex items-center gap-2 text-gray-400 font-medium">
+            Car Type
+          </Label>
+          <Select.Trigger className="border border-gray-600 focus:border-orange-500">
+            <Select.Value />
+            <Select.Indicator />
+          </Select.Trigger>
+          <Select.Popover>
+            <ListBox>
+              <ListBox.Item id="suv" textValue="SUV">
+                SUV
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+              <ListBox.Item id="sedan" textValue="Sedan">
+                Sedan
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+              <ListBox.Item id="coupe" textValue="Coupe">
+                Coupe
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+              <ListBox.Item id="hatchback" textValue="Hatchback">
+                Hatchback
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+              <ListBox.Item id="convertible" textValue="Convertible">
+                Convertible
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+              <ListBox.Item id="crossover" textValue="Crossover">
+                Crossover
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+            </ListBox>
+          </Select.Popover>
+        </Select>
+      </div>
+      <Button
+        type="submit"
+        className="bg-orange-500 text-black rounded-xl w-full mt-6 btn"
+      >
+        Search
+      </Button>
+    </Form>
+  );
+};
+
+export default FilterForm;
